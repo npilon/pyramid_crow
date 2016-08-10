757cc50def8ae5d0fa6e24b173450c7a9afdf531
Gavin Carothers <gavin@carothers.name>
2016-08-10 13:41:03 -0700

    Inital support for matrix build on travis
    Expecting all 3.x tests to fail

--- setup.py
+++ setup.py
@@ -3,6 +3,21 @@ import sys, os
 
 version = '0.3.1'
 
+install_requires=[
+    'pyramid>=1.2dev',
+    'raven>=5.20.0',
+],
+
+tests_require = [
+    'WebTest >= 2.0.21',
+    'mock >=2.0.0',
+    ]
+
+testing_extras = tests_require + [
+    'nose',
+    'coverage',
+    ]
+
 setup(name='pyramid_crow',
       version=version,
       description="http context compliant automatic raven integration for pyramid",
@@ -17,9 +32,9 @@ setup(name='pyramid_crow',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-          'pyramid>=1.2dev',
-          'raven>=5.20.0',
-      ],
+      install_requires=install_requires,
+      extras_require={
+          'testing': testing_extras,
+      },
+      tests_require=tests_require,
       )
